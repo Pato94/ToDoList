e641132d09591f96022b479b5a700ac9d56dab10
Patricio Contreras <patricio@pigmalionstudios.com.ar>
2015-04-10 17:52:18 -0300

    oaeirgjoie

--- app/src/main/java/com/pigmalionstudios/todolist/MainActivity.java
+++ app/src/main/java/com/pigmalionstudios/todolist/MainActivity.java
@@ -2,11 +2,19 @@ package com.pigmalionstudios.todolist;
 
 
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.drive.Drive;
 
 import java.util.ArrayList;
 
@@ -17,8 +25,15 @@ public class MainActivity extends ActionBarActivity {
     public static final int COMPLETAR = 6;
     public TareasDataSource datasource;
     public ArrayList tareasPendientes;
-    public ListaFragment fragmentoVivo;
-    GoogleApiClient googleApiClient;
+    GoogleApiClient mGoogleApiClient;
+
+    private boolean mSignInClicked;
+
+    /**
+     * True if we are in the process of resolving a ConnectionResult
+     */
+    private boolean mIntentInProgress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -27,7 +42,10 @@ public class MainActivity extends ActionBarActivity {
 
         datasource= new TareasDataSource(this);
         datasource.open();
-
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .build();
         tareasPendientes = new ArrayList<Tarea>();
         ActionBar actionBar = getSupportActionBar();
 //        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
@@ -51,7 +69,35 @@ public class MainActivity extends ActionBarActivity {
         actionBar.addTab(tabPendientes);
         actionBar.addTab(tabHechas);
     }
+    /**
+     * True if the sign-in button was clicked.  When true, we know to resolve all
+     * issues preventing sign-in without waiting.
+     */
+    private boolean mSignInClicked;
 
+    /**
+     * True if we are in the process of resolving a ConnectionResult
+     */
+    private boolean mIntentInProgress;
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (!mIntentInProgress) {
+            if (mSignInClicked && result.hasResolution()) {
+                // The user has already clicked 'sign-in' so we attempt to resolve all
+                // errors until the user is signed in, or they cancel.
+                try {
+                    result.startResolutionForResult(this, RC_SIGN_IN);
+                    mIntentInProgress = true;
+                } catch (IntentSender.SendIntentException e) {
+                    // The intent was canceled before it was sent.  Return to the default
+                    // state and attempt to connect to get an updated ConnectionResult.
+                    mIntentInProgress = false;
+                    mGoogleApiClient.connect();
+                }
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -71,6 +117,10 @@ public class MainActivity extends ActionBarActivity {
             if (getSupportActionBar().getSelectedTab().getText().toString().equals(getString(R.string.tag_pendientes)))
                     crearNuevaTarea("");
         }
+        if (id==R.id.action_login){
+            mSignInClicked = true;
+            mGoogleApiClient.connect();
+        }
         //noinspection SimplifiableIfStatement
 
 
@@ -80,44 +130,60 @@ public class MainActivity extends ActionBarActivity {
         Intent intent = new Intent(this, DetalleActivity.class);
         intent.putExtra("nombre", nombre);
         intent.putExtra("razon", DetalleActivity.CREAR);
-        startActivityForResult(intent, 3);
+        startActivityForResult(intent, 73);
+    }
+
+    public void onConnected(Bundle connectionHint) {
+        mSignInClicked = false;
+        Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
     }
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {//Estaria bueno que le de uso al request code
-        if (RESULT_OK == resultCode)
-        {
-            ListaFragment fragmentPendientes = (ListaFragment)(getSupportFragmentManager().findFragmentByTag("Pendientes"));
-            ListaFragment  fragmentHechas = (ListaFragment)(getSupportFragmentManager().findFragmentByTag("Hechas"));
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {//Estaria bueno que le de uso al request code
+        if (requestCode != 73) {
+            if (resultCode != RESULT_OK) {
+                mSignInClicked = false;
+            }
 
-            int razon = data.getIntExtra("razon", -1);
-            if (razon==BORRAR){
+            mIntentInProgress = false;
 
-                //String nombre = data.getStringExtra("nombre");
-                Tarea tarea = data.getParcelableExtra("tarea");
+            if (!mGoogleApiClient.isConnected()) {
+                mGoogleApiClient.reconnect();
+            }
+        } else {
+            if (RESULT_OK == resultCode) {
+                ListaFragment fragmentPendientes = (ListaFragment) (getSupportFragmentManager().findFragmentByTag("Pendientes"));
+                ListaFragment fragmentHechas = (ListaFragment) (getSupportFragmentManager().findFragmentByTag("Hechas"));
+
+                int razon = data.getIntExtra("razon", -1);
+                if (razon == BORRAR) {
+
+                    //String nombre = data.getStringExtra("nombre");
+                    Tarea tarea = data.getParcelableExtra("tarea");
 //                String tag = getSupportFragmentManager().getBackStackEntryAt(getSupportFragmentManager().getBackStackEntryCount() - 1).getName();
-                fragmentPendientes.borrarPorNombre(tarea.getNombre());
-            }
-            if (razon==AGREGAR){
-                String nombre = data.getStringExtra("nombre");
-                boolean esAgregar = data.getStringExtra("esAgregar").equals("si");
-                Fecha fechaAlarma = data.getParcelableExtra("fechaAlarma");
-                Fecha fechaLimite = data.getParcelableExtra("fechaLimite");
-                fechaAlarma.mostrarFecha();
-                fechaLimite.mostrarFecha();
-                //Tirar null null es OBVIAMENTE una burrada TODO
-                if(!fragmentPendientes.agregarALista(new Tarea(nombre, fechaAlarma, fechaLimite, false), esAgregar)){
-                    crearNuevaTarea(nombre);//Para que llame a la instancia otra vez
-                };
-            }
-            if (razon==COMPLETAR){
-                String nombre = data.getStringExtra("nombre");
-                //Fue un intento fallido por hacer que funcione
+                    fragmentPendientes.borrarPorNombre(tarea.getNombre());
+                }
+                if (razon == AGREGAR) {
+                    String nombre = data.getStringExtra("nombre");
+                    boolean esAgregar = data.getStringExtra("esAgregar").equals("si");
+                    Fecha fechaAlarma = data.getParcelableExtra("fechaAlarma");
+                    Fecha fechaLimite = data.getParcelableExtra("fechaLimite");
+                    fechaAlarma.mostrarFecha();
+                    fechaLimite.mostrarFecha();
+                    //Tirar null null es OBVIAMENTE una burrada TODO
+                    if (!fragmentPendientes.agregarALista(new Tarea(nombre, fechaAlarma, fechaLimite, false), esAgregar)) {
+                        crearNuevaTarea(nombre);//Para que llame a la instancia otra vez
+                    }
+                    ;
+                }
+                if (razon == COMPLETAR) {
+                    String nombre = data.getStringExtra("nombre");
+                    //Fue un intento fallido por hacer que funcione
 //                getSupportFragmentManager().beginTransaction().replace(android.R.id.content, fragmentHechas, "Hechas33");
-                //fragmentHechas.agregarALista(fragmentPendientes.borrarPorNombre(nombre), false);
-                Tarea tarea = fragmentPendientes.borrarPorNombre(nombre);
+                    //fragmentHechas.agregarALista(fragmentPendientes.borrarPorNombre(nombre), false);
+                    Tarea tarea = fragmentPendientes.borrarPorNombre(nombre);
 //                fragmentHechas.agregarALista(tarea, true);
-                tareasPendientes.add(tarea.hacete());
+                    tareasPendientes.add(tarea.hacete());
+                }
             }
         }
     }
